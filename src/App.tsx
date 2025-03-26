d4379c2ffaf0d357d53a02ea5ead0e39c43873b1
andriputra <andriputra.job@gmail.com>
2025-03-26 12:39:48 +0700

    modified app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import AppNavigator from './navigation/AppNavigator';
+import { enableScreens } from 'react-native-screens';
+enableScreens();
 
 const App = () => {
   return (
